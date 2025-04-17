ff25f893cdd87ec0fa3c36478211219ea4bac86d
Elbarand <206485242+Elbarand@users.noreply.github.com>
2025-04-18 03:39:42 +0900

    Update rumor.rb

--- whispers/models/rumor.rb
+++ whispers/models/rumor.rb
@@ -1,21 +1,45 @@
 class Rumor
-  attr_reader :id, :character_name, :content, :timestamp, :reactions
+  attr_reader :id, :character_name, :content, :timestamp, :influence_level
 
   def initialize(attributes = {})
     @id = attributes[:id]
     @character_name = attributes[:character_name]
     @content = attributes[:content]
     @timestamp = attributes[:timestamp] || Time.now
-    @reactions = []
+    @influence_level = calculate_influence(attributes[:content])
+    @delivered_to = Set.new # 이미 전달된 플레이어 추적
   end
 
-  def self.create(attributes)
-    rumor = new(attributes)
-    # DB 저장 로직
-    rumor
+  def deliver_to_player(player_id)
+    return false if @delivered_to.include?(player_id)
+    
+    @delivered_to.add(player_id)
+    apply_influence(player_id)
+    true
   end
 
-  def format_display
-    "[#{@character_name}] #{@content}"
+  private
+
+  def calculate_influence(content)
+    # 소문의 영향력을 계산 (1-10 스케일)
+    # 키워드나 문장 길이 등을 기반으로 계산
+    base_influence = 5
+    
+    # 중요 키워드에 따른 영향력 조정
+    keywords = ["비밀", "위험", "금지", "마법", "어둠"]
+    keyword_count = keywords.count { |word| content.include?(word) }
+    
+    base_influence + keyword_count
+  end
+
+  def apply_influence(player_id)
+    influence = {
+      rumor_id: @id,
+      character_name: @character_name,
+      influence_level: @influence_level,
+      timestamp: @timestamp
+    }
+    
+    PlayerInfluenceTracker.record(player_id, influence)
   end
 end
