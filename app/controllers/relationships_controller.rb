7bbb05d88eadef32784c54a4c3c2bb13e1240b96
Elbarand <206485242+Elbarand@users.noreply.github.com>
2025-04-30 17:27:42 +0900

    relationships_controller.rb 업데이트

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,69 +1,17 @@
-# frozen_string_literal: true
+# app/controllers/registrations_controller.rb
+class RegistrationsController < Devise::RegistrationsController
+  def create
+    invite_code = InviteCode.find_by(code: params[:invite_code])
 
-class RelationshipsController < ApplicationController
-  layout 'admin'
+    unless invite_code && !invite_code.used?
+      flash[:alert] = '유효하지 않은 초대 코드입니다.'
+      redirect_to new_user_registration_path and return
+    end
 
-  before_action :authenticate_user!
-  before_action :set_accounts, only: :show
-  before_action :set_relationships, only: :show
-
-  helper_method :following_relationship?, :followed_by_relationship?, :mutual_relationship?
-
-  def show
-    @form = Form::AccountBatch.new
-  end
-
-  def update
-    @form = Form::AccountBatch.new(form_account_batch_params.merge(current_account: current_account, action: action_from_button))
-    @form.save
-  rescue ActionController::ParameterMissing
-    # Do nothing
-  rescue Mastodon::NotPermittedError, ActiveRecord::RecordNotFound
-    flash[:alert] = I18n.t('relationships.follow_failure') if action_from_button == 'follow'
-  ensure
-    redirect_to relationships_path(filter_params)
-  end
-
-  private
-
-  def set_accounts
-    @accounts = RelationshipFilter.new(current_account, filter_params).results.page(params[:page]).per(40)
-  end
-
-  def set_relationships
-    @relationships = AccountRelationshipsPresenter.new(@accounts, current_user.account_id)
-  end
-
-  def form_account_batch_params
-    params.require(:form_account_batch).permit(:action, account_ids: [])
-  end
-
-  def following_relationship?
-    params[:relationship].blank? || params[:relationship] == 'following'
-  end
-
-  def mutual_relationship?
-    params[:relationship] == 'mutual'
-  end
-
-  def followed_by_relationship?
-    params[:relationship] == 'followed_by'
-  end
-
-  def filter_params
-    params.slice(:page, *RelationshipFilter::KEYS).permit(:page, *RelationshipFilter::KEYS)
-  end
-
-  def action_from_button
-    case
-    when params[:follow]
-      'follow'
-    when params[:unfollow]
-      'unfollow'
-    when params[:remove_from_followers]
-      'remove_from_followers'
-    when params[:block_domains], params[:remove_domains_from_followers]
-      'remove_domains_from_followers'
+    super do |resource|
+      if resource.persisted?
+        invite_code.update(used: true, user: resource)
+      end
     end
   end
 end
