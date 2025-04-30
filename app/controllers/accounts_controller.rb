bfe71b4c086b21703f7bd9b0e9762f0d984a8dbb
Elbarand <206485242+Elbarand@users.noreply.github.com>
2025-04-30 18:31:13 +0900

    accounts_controller.rb 업데이트

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,100 +1,31 @@
 # frozen_string_literal: true
 
-class AccountsController < ApplicationController
-  include SignatureVerification
-  include AccountControllerConcern
-  include WebAppControllerConcern
-  include Authorization
+class Admin::AccountsController < Admin::BaseController
+  before_action :set_account, only: [:show, :statuses, :dms]
 
-  before_action :set_account
-  before_action :check_account_suspension
-  before_action :authenticate_user!, if: :limited_federation_mode?
-  before_action :set_instance_presenter
-  before_action :redirect_new_user
-  before_action :set_tab
-  before_action :set_statuses
-  before_action :set_media_attachments, if: -> { @tab == 'media' }
-  before_action :set_tag, if: -> { @tab == 'tagged' }
-  before_action :set_search_params, if: -> { @tab == 'search' }
-  before_action :redirect_silo_mode_if_needed, only: [:show]
-
-  layout 'public'
+  def index
+    authorize :account, :index?
+    @accounts = Account.search(params[:q], limit: 50)
+  end
 
   def show
-    respond_to do |format|
-      format.html do
-        expires_in(15.seconds, public: true, stale_while_revalidate: 30.seconds, stale_if_error: 1.hour) unless user_signed_in?
-        render :show
-      end
+    authorize @account, :show?
+  end
 
-      format.rss do
-        expires_in 0, public: true
-        render layout: false
-      end
+  def statuses
+    authorize @account, :show?
+    @statuses = @account.statuses.order(id: :desc).page(params[:page]).per(20)
+  end
 
-      format.json do
-        expires_in 3.minutes, public: public_fetch_mode?
-        render json: @account, serializer: REST::AccountSerializer, relationships: AccountRelationshipsPresenter.new([@account], current_user&.account_id), include: ''
-      end
-    end
+  # ✅ 추가된 관리자용 DM 조회 액션
+  def dms
+    authorize @account, :show?
+    @dms = @account.statuses.where(visibility: :direct).order(id: :desc).page(params[:page]).per(20)
   end
 
   private
 
   def set_account
-    @account = Account.find_local!(params[:username])
-  end
-
-  def check_account_suspension
-    gone if @account.suspended?
-  end
-
-  def redirect_new_user
-    redirect_to about_path if !user_signed_in? && !@account.discoverable?
-  end
-
-  def set_tab
-    @tab = %w[media with_replies tagged search].include?(params[:tab]) ? params[:tab] : 'statuses'
-  end
-
-  def set_instance_presenter
-    @instance_presenter = InstancePresenter.new
-  end
-
-  def set_statuses
-    @statuses = case @tab
-    when 'media'
-      []
-    when 'with_replies'
-      @account.statuses.with_replies
-    when 'tagged'
-      @account.statuses.tagged_with(params[:tag])
-    when 'search'
-      []
-    else
-      @account.statuses.where(visibility: [:public, :unlisted])
-    end
-
-    @statuses = @statuses.where.not(visibility: :direct) if Rails.configuration.x.chuchu.silo_mode
-    @statuses = @statuses.limit(20).to_a if @statuses
-  end
-
-  def set_media_attachments
-    @media_attachments = @account.media_attachments.attached.limit(10)
-  end
-
-  def set_tag
-    @tag = Tag.find_by_name(params[:tag])
-  end
-
-  def set_search_params
-    @search_query = params[:q]
-  end
-
-  def redirect_silo_mode_if_needed
-    return unless Rails.configuration.x.chuchu.silo_mode
-    return if user_signed_in?
-
-    redirect_to new_user_session_path, alert: '로그인한 사용자만 계정 페이지를 볼 수 있습니다.'
+    @account = Account.find(params[:id])
   end
 end
