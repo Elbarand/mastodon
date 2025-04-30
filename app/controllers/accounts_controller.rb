50f620205e23f3bcb12981b5b982125ec90a9159
Elbarand <206485242+Elbarand@users.noreply.github.com>
2025-04-30 18:17:06 +0900

    accounts_controller.rb 업데이트

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,18 +1,47 @@
 # frozen_string_literal: true
 
 class AccountsController < ApplicationController
+  include AccountControllerConcern
+  include SignatureVerification
+  include RateLimitHeaders
+
   layout 'public'
 
-  before_action :authenticate_user! # 모든 사용자 페이지 접근 차단
   before_action :set_account
+  before_action :check_account_suspension
+  before_action :set_cache_headers
+  before_action :check_chuchu_mode!
 
   def show
-    # 기본 계정 페이지
+    respond_to do |format|
+      format.html do
+        expires_in(0, public: true) if user_signed_in?
+        fresh_when(etag: @account, last_modified: @account.updated_at)
+      end
+
+      format.json do
+        render json: @account, serializer: REST::AccountSerializer
+      end
+    end
   end
 
   private
 
   def set_account
     @account = Account.find_local!(params[:username])
+  end
+
+  def set_cache_headers
+    response.headers['Cache-Control'] = 'no-store'
+  end
+
+  def check_account_suspension
+    not_found if @account.suspended?
+  end
+
+  def check_chuchu_mode!
+    if Rails.configuration.x.chuchu.silo_mode && !user_signed_in?
+      not_found
+    end
   end
 end
