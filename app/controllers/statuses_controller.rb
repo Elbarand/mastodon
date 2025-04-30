5a2585b7618f820d1938cc6c1d0799c476b7de35
Elbarand <206485242+Elbarand@users.noreply.github.com>
2025-04-30 17:44:07 +0900

    statuses_controller.rb 업데이트

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,69 +1,27 @@
 # frozen_string_literal: true
 
 class StatusesController < ApplicationController
-  include WebAppControllerConcern
-  include SignatureAuthentication
-  include Authorization
-  include AccountOwnedConcern
+  layout 'public'
 
-  vary_by -> { public_fetch_mode? ? 'Accept, Accept-Language, Cookie' : 'Accept, Accept-Language, Cookie, Signature' }
-
-  before_action :authenticate_user!, only: [:show, :activity], if: -> { request.format == :json && authorized_fetch_mode? }
+  before_action :authenticate_user! # 로그인 안 하면 접근 불가
   before_action :set_status
-  before_action :redirect_to_original, only: :show
-
-  after_action :set_link_headers
-
-  skip_around_action :set_locale, if: -> { request.format == :json }
-  skip_before_action :require_functional!, only: [:show, :embed], unless: :limited_federation_mode?
-
-  content_security_policy only: :embed do |policy|
-    policy.frame_ancestors(false)
-  end
+  before_action :set_cache_headers
 
   def show
-    respond_to do |format|
-      format.html do
-        expires_in 10.seconds, public: true if current_account.nil?
-      end
-
-      format.json do
-        expires_in 3.minutes, public: true if @status.distributable? && public_fetch_mode?
-        render_with_cache json: @status, content_type: 'application/activity+json', serializer: ActivityPub::NoteSerializer, adapter: ActivityPub::Adapter
-      end
-    end
-  end
-
-  def activity
-    expires_in 3.minutes, public: @status.distributable? && public_fetch_mode?
-    render_with_cache json: ActivityPub::ActivityPresenter.from_status(@status), content_type: 'application/activity+json', serializer: ActivityPub::ActivitySerializer, adapter: ActivityPub::Adapter
+    expires_in(3.minutes, public: true)
   end
 
   def embed
-    return not_found if @status.hidden? || @status.reblog?
-
-    expires_in 180, public: true
-    response.headers.delete('X-Frame-Options')
-
-    render layout: 'embedded'
+    render layout: 'embed'
   end
 
   private
 
-  def set_link_headers
-    response.headers['Link'] = LinkHeader.new(
-      [[ActivityPub::TagManager.instance.uri_for(@status), [%w(rel alternate), %w(type application/activity+json)]]]
-    ).to_s
-  end
-
   def set_status
-    @status = @account.statuses.find(params[:id])
-    authorize @status, :show?
-  rescue Mastodon::NotPermittedError
-    not_found
+    @status = Status.find(params[:id])
   end
 
-  def redirect_to_original
-    redirect_to(ActivityPub::TagManager.instance.url_for(@status.reblog), allow_other_host: true) if @status.reblog?
+  def set_cache_headers
+    response.headers['Cache-Control'] = 'no-store'
   end
 end
