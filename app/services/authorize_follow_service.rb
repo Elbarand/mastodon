6409087b53a5dcf94572ff25f030a444982f33d9
Elbarand <206485242+Elbarand@users.noreply.github.com>
2025-04-30 18:07:27 +0900

    authorize_follow_service.rb 업데이트

--- app/services/authorize_follow_service.rb
+++ app/services/authorize_follow_service.rb
@@ -1,27 +1,13 @@
 # frozen_string_literal: true
 
 class AuthorizeFollowService < BaseService
-  include Payloadable
+  def call(target_account, source_account, message = nil)
+    return nil if Rails.configuration.x.whip.silo_mode
 
-  def call(source_account, target_account, **options)
-    if options[:skip_follow_request]
-      follow_request = FollowRequest.new(account: source_account, target_account: target_account, uri: options[:follow_request_uri])
-    else
-      follow_request = FollowRequest.find_by!(account: source_account, target_account: target_account)
-      follow_request.authorize!
-    end
+    return nil unless target_account.locked?
 
-    create_notification(follow_request) if !source_account.local? && source_account.activitypub?
+    follow_request = FollowRequest.create!(account: source_account, target_account: target_account)
+    ActivityPub::DeliveryWorker.perform_async(follow_request.id, source_account.id, 'Follow')
     follow_request
-  end
-
-  private
-
-  def create_notification(follow_request)
-    ActivityPub::DeliveryWorker.perform_async(build_json(follow_request), follow_request.target_account_id, follow_request.account.inbox_url)
-  end
-
-  def build_json(follow_request)
-    Oj.dump(serialize_payload(follow_request, ActivityPub::AcceptFollowSerializer))
   end
 end
