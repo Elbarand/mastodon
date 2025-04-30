c4b7fdb64d04271cec0047f38a3ed37158eb1365
Elbarand <206485242+Elbarand@users.noreply.github.com>
2025-04-30 18:15:45 +0900

    statuses_controller.rb 업데이트

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,18 +1,27 @@
 # frozen_string_literal: true
 
 class StatusesController < ApplicationController
+  include SignatureVerification
+  include RateLimitHeaders
+
   layout 'public'
 
-  before_action :authenticate_user! # 로그인 안 하면 접근 불가
   before_action :set_status
   before_action :set_cache_headers
+  before_action :set_link_headers
+  before_action :check_silenced_account
+  before_action :check_chuchu_mode!
 
   def show
-    expires_in(3.minutes, public: true)
-  end
-
-  def embed
-    render layout: 'embed'
+    respond_to do |format|
+      format.html do
+        expires_in(0, public: true) if user_signed_in?
+        fresh_when(etag: @status, last_modified: @status.updated_at)
+      end
+      format.json do
+        render json: @status, serializer: REST::StatusSerializer
+      end
+    end
   end
 
   private
@@ -23,5 +32,21 @@ class StatusesController < ApplicationController
 
   def set_cache_headers
     response.headers['Cache-Control'] = 'no-store'
+  end
+
+  def set_link_headers
+    return unless @status.thread?
+
+    response.headers['Link'] = "<#{short_account_status_url(@status.account, @status)}>; rel=\"canonical\""
+  end
+
+  def check_silenced_account
+    not_found if @status.account.silenced?
+  end
+
+  def check_chuchu_mode!
+    if Rails.configuration.x.chuchu.silo_mode && !user_signed_in?
+      not_found
+    end
   end
 end
