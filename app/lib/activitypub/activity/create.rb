939e9f2f82bba544b37451729c8ffac22e65dff0
Elbarand <206485242+Elbarand@users.noreply.github.com>
2025-04-30 17:14:18 +0900

    create.rb 업데이트

--- app/lib/activitypub/activity/create.rb
+++ app/lib/activitypub/activity/create.rb
@@ -1,28 +1,14 @@
 # frozen_string_literal: true
 
-class AccountsController < ApplicationController
-  layout 'public'
+module ActivityPub
+  module Activity
+    class Create < Base
+      include JsonLdHelper
 
-  before_action :set_account
-  before_action :set_instance_presenter
-  before_action :authenticate_user!, only: [:show]
-
-  def show
-    @statuses = @account.statuses
-                        .without_reblogs
-                        .without_replies
-                        .where(visibility: [:public, :unlisted])
-                        .limit(20)
-                        .to_a
-  end
-
-  private
-
-  def set_account
-    @account = Account.find_local!(params[:username])
-  end
-
-  def set_instance_presenter
-    @instance_presenter = InstancePresenter.new
+      def perform
+        ActivityPub 전송을 무시하고 툿 저장만 수행
+        Rails.logger.info "ActivityPub 전파 차단: #{status.id}"
+      end
+    end
   end
 end
